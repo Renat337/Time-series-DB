1d82b900bd5528d02e60f65b2f694e0c63fee590
Renat337 <renat.ren@yahoo.com>
2024-11-13 15:35:05 +0000

    Features planned

---
+++ General.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+path = pd.read_csv("SPX.csv")
+
+def missingvals(column):
+    pass
+
+def datacleaning():
+    pass
+
+def predict():
+    pass
+
+def plot(column):
+    plt.plot(path)
